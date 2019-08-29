5565da4903c0c9a497bca33023daccff3c3dcea6
Gustavo Silva <gustavo.silva@codigree.com>
2019-08-29 15:13:33 +0100

    Moves to Go modules and use a different SSE package

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ubuconeurope/present-switch
+
+go 1.12
+
+require github.com/alexandrevicenzi/go-sse v0.0.0-20190531224209-805eefa457e7
--- main.go
+++ main.go
@@ -1,127 +1,57 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/alexandrevicenzi/go-sse"
 )
 
-// Loads the template
-// TODO add ways to dynamically set next conf name and etup
-func loadTemplate(content ...string) *template.Template {
-	// Read in the template with our SSE JavaScript code.
-	t, err := template.ParseFiles("templates/index.html")
-	if err != nil {
-		log.Fatal("Errors parsing your template file. Fix it and try again.")
-	}
-	return t
-}
-
-// Handler for the main page, which we wire up to the route at "/" below in `main`.
-// TODO ensure it loads only by ID
-func contentGetter(w http.ResponseWriter, r *http.Request, b *Broker) {
-	var rid = strings.TrimPrefix(r.URL.Path, "/rooms/")
-	if _, err := strconv.ParseInt(rid, 10, 8); err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	t := loadTemplate(rid)
-	t.Execute(w, "friend")
-	pushMessage(b, *t, rid)
-	log.Println("Finished HTTP request at", r.URL.Path)
-}
-
-// Method that controls modifying the content of a template.
-// First it attempts to modify it and then it will push the message to the output.
-func changeContent(w http.ResponseWriter, r *http.Request, b *Broker) {
-	var rid = strings.TrimPrefix(r.URL.Path, "/rooms/")
-	var body = parseBody(r)
-	ReplaceInTemplate(rid, body.convertToMap())
-
-	t := loadTemplate(rid)
-	t.Execute(w, "friend")
-	pushMessage(b, *t, rid)
-	log.Println("Finished HTTP Request at", r.URL.Path)
-}
-
-// Method to parse body and handle its errors separately.
-func parseBody(r *http.Request) RequestBody {
-	var rb RequestBody
-	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&rb)
-	if err != nil {
-		panic(err)
-	}
-
-	return rb
-}
-
-func pushMessage(b *Broker, t template.Template, rid string) {
-	var tpl bytes.Buffer
-	t.Execute(&tpl, strings.Join([]string{"room"}, rid))
-	b.messages <- tpl.String()
-}
-
-// Main routine
 func main() {
-	// Make a new Broker instance
-	b := &Broker{
-		make(map[chan string]bool),
-		make(chan (chan string)),
-		make(chan (chan string)),
-		make(chan string),
-	}
+	s := sse.NewServer(&sse.Options{
+		// CORS headers
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin":  "*",
+			"Access-Control-Allow-Methods": "GET, OPTIONS",
+			"Access-Control-Allow-Headers": "Keep-Alive,X-Requested-With,Cache-Control,Content-Type,Last-Event-ID",
+		},
+		// Custom channel name generator
+		ChannelNameFunc: func(request *http.Request) string {
+			return request.URL.Path
+		},
+		Logger: log.New(os.Stdout, "go-sse: ", log.Ldate|log.Ltime|log.Lshortfile),
+	})
+	defer s.Shutdown()
 
-	// Start processing events
-	b.Start()
-
-	// Make b the HTTP handler for "/events/".  It can do
-	// this because it has a ServeHTTP method.  That method
-	// is called in a separate goroutine for each
-	// request to "/events/".
-	http.Handle("/events/", b)
-
-	// Routing handler
 	http.HandleFunc("/rooms/", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			contentGetter(w, r, b)
-			return
-		case http.MethodPost:
-			changeContent(w, r, b)
-			return
-		default:
-			w.WriteHeader(http.StatusMethodNotAllowed)
-			return
-		}
+		var rid = strings.TrimPrefix(r.URL.Path, "/rooms/")
+		http.FileServer(http.Dir(fmt.Sprintf("./templates/%s/static_files/", rid)))
 	})
 
-	// This function sends data for the message chan every 5seconds.
+	// Register /events endpoint
+	http.Handle("/events/", s)
+
 	go func() {
-		for i := 0; ; i++ {
-
-			// Create a little message to send to clients,
-			// including the current time.
-			b.messages <- fmt.Sprintf("TEST MESSAGE: %d - the time is: <br>%v", i, time.Now())
-
-			// Print a nice log message and sleep for 5s.
-			log.Printf("Sent message %d ", i)
-			time.Sleep(5e9)
-
+		for {
+			s.SendMessage("/events/channel-1", sse.SimpleMessage(time.Now().String()))
+			time.Sleep(5 * time.Second)
 		}
 	}()
 
-	// Routing handler
-	http.Handle("/", http.FileServer(http.Dir("static_files")))
+	go func() {
+		i := 0
+		for {
+			i++
+			s.SendMessage("/events/channel-2", sse.SimpleMessage(strconv.Itoa(i)))
+			time.Sleep(5 * time.Second)
+		}
+	}()
 
-	// Start the server and listen forever on port 8000.
-	log.Println("Starting...")
-	http.ListenAndServe(":8000", nil)
-	log.Print("Server staretd listening to new clients.")
+	log.Println("Listening at :3000")
+	http.ListenAndServe(":3000", nil)
 }
