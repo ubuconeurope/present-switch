e2302579f9dd5342295576c977da79c9261cd432
Tiago Carreira <>
2019-09-09 11:52:21 +0100

    add persistence (not used yet)

---
+++ persistence.go
@@ -0,0 +1,131 @@
+package presentswitch
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// Represents the current state of a RoomInfo.
+type RoomInfo struct {
+	ID             int // room number
+	RoomName       string
+	CurrentTitle   string
+	CurrentSpeaker string
+	CurrentTime    string
+	NextTitle      string
+	NextSpeaker    string
+	NextTime       string
+}
+
+// InitDB creates a new DB object using filename as parameter
+func InitDB(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+	if err != nil {
+		panic(err)
+	}
+	if db == nil {
+		panic("db nil")
+	}
+	return db
+}
+
+// CreateTables create all tables needed.
+func CreateTables(db *sql.DB) {
+	// create table if not exists
+	sqlTable := `
+	CREATE TABLE IF NOT EXISTS room_info(
+		id INTEGER PRIMARY KEY,
+		room_name TEXT NOT NULL,
+		current_title TEXT NOT NULL,
+		current_speaker TEXT NOT NULL,
+		current_time TEXT NOT NULL,
+		next_title TEXT NOT NULL,
+		next_speaker TEXT NOT NULL,
+		next_time TEXT NOT NULL
+	);
+	`
+
+	_, err := db.Exec(sqlTable)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// StoreItem stores multiple items
+func StoreItem(db *sql.DB, item RoomInfo) {
+	sqlAdditem := `
+	INSERT OR REPLACE INTO room_info(
+		id,
+		room_name,
+		current_title,
+		current_speaker,
+		current_time,
+		next_title,
+		next_speaker,
+		next_time
+	) values(?, ?, ?, ?, ?, ?, ?, ?)
+	`
+
+	stmt, err := db.Prepare(sqlAdditem)
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+
+	_, err2 := stmt.Exec(item.ID, item.RoomName,
+		item.CurrentTitle, item.CurrentSpeaker, item.CurrentTime,
+		item.NextTitle, item.NextSpeaker, item.NextTime)
+	if err2 != nil {
+		panic(err2)
+
+	}
+}
+
+// ReadRoomInfoTable reads all rows in the room_info table
+func ReadRoomInfoTable(db *sql.DB) []RoomInfo {
+	sqlReadall := `
+	SELECT * FROM room_info
+	ORDER BY id DESC
+	`
+
+	rows, err := db.Query(sqlReadall)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var result []RoomInfo
+	for rows.Next() {
+		item := RoomInfo{}
+		err2 := rows.Scan(&item.ID, &item.RoomName,
+			&item.CurrentTitle, &item.CurrentSpeaker, &item.CurrentTime,
+			&item.NextTitle, &item.NextSpeaker, &item.NextTime)
+		if err2 != nil {
+			panic(err2)
+		}
+		result = append(result, item)
+	}
+	return result
+}
+
+// ReadRoomInfo read one row, with the row ID
+func ReadRoomInfo(db *sql.DB, id int) (RoomInfo, error) {
+	sqlRead := `
+	SELECT * FROM room_info
+	WHERE id = ?
+	`
+
+	stmt, err := db.Prepare(sqlRead)
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+
+	var item RoomInfo
+	err = stmt.QueryRow(id).Scan(&item.ID, &item.RoomName,
+		&item.CurrentTitle, &item.CurrentSpeaker, &item.CurrentTime,
+		&item.NextTitle, &item.NextSpeaker, &item.NextTime)
+
+	return item, err
+}
---
+++ persistence_test.go
@@ -0,0 +1,78 @@
+package presentswitch
+
+import (
+	"os"
+	"testing"
+)
+
+// All tests in the same tests because I need state and it is easier
+func TestAllPersistence(t *testing.T) {
+	const dbpath = "foo.db"
+	var readItems []RoomInfo
+	var (
+		readSingleItem RoomInfo
+		err            error
+	)
+
+	// test 0 - init db
+	db := InitDB(dbpath)
+	defer db.Close()
+	defer os.Remove(dbpath)
+
+	// test 1 - create tables
+	CreateTables(db)
+	t.Log("TestAllPersistence - TEST1 - CreateTables - OK")
+
+	// test 2 - insert data
+	StoreItem(db, RoomInfo{1, "Room1", "CurrTitle", "CurrSpeaker", "CurrTime", "NextTitle", "NextSpeaker", "NextTime"})
+
+	readItems = ReadRoomInfoTable(db)
+	if 1 != len(readItems) {
+		t.Errorf("Number of rows did not match the expected number 1")
+	}
+
+	// test 2.1 - insert more data
+	StoreItem(db, RoomInfo{2, "Room2", "CurrTitle", "CurrSpeaker", "CurrTime", "NextTitle", "NextSpeaker", "NextTime"})
+	StoreItem(db, RoomInfo{3, "Room4", "CurrTitle", "CurrSpeaker", "CurrTime", "NextTitle", "NextSpeaker", "NextTime"})
+	StoreItem(db, RoomInfo{4, "Room4", "CurrTitle", "CurrSpeaker", "CurrTime", "NextTitle", "NextSpeaker", "NextTime"})
+
+	readItems = ReadRoomInfoTable(db)
+	if 4 != len(readItems) {
+		t.Errorf("Number of rows did not match the expected number 1")
+	}
+	t.Log("TestAllPersistence - TEST2 - insert data - ", len(readItems), "(<-- 4 is expected)")
+
+	// test 3 - retrieve single data
+	readSingleItem, err = ReadRoomInfo(db, 2)
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+	if "Room2" != readSingleItem.RoomName {
+		t.Errorf("Unexpected RoomName: %s", readSingleItem.RoomName)
+	}
+	t.Log("TestAllPersistence - TEST3 - retrieve single data - ", readSingleItem)
+
+	// test 4 - retrieve unexisting data
+	readSingleItem, err = ReadRoomInfo(db, 99)
+	if err != nil {
+		// simple error is nice here.
+		if err.Error() != "sql: no rows in result set" {
+			t.Error("Unexpected error: ", err)
+		}
+	}
+	t.Log("TestAllPersistence - TEST4 - retrieve unexisting data - ", err)
+
+	// test 5 - update row
+	StoreItem(db, RoomInfo{2, "Room2", "Another Title Here", "", "", "", "", ""})
+	readSingleItem, err = ReadRoomInfo(db, 2)
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+	if "Another Title Here" != readSingleItem.CurrentTitle {
+		t.Errorf("Unexpected RoomName: %s", readSingleItem.RoomName)
+	}
+	t.Log("TestAllPersistence - TEST5 - update row - ", readSingleItem)
+
+}
